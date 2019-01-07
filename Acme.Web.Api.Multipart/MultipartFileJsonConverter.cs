691631c89cf85ea83d2fa46902ec628f559a9878
Olivier Bossaer <olivier.bossaer@gmail.com>
2019-01-07 09:13:13 +0100

    Missing WriteJson

--- Acme.Web.Api.Multipart/MultipartFileJsonConverter.cs
+++ Acme.Web.Api.Multipart/MultipartFileJsonConverter.cs
@@ -63,5 +63,16 @@ namespace Acme.Web.Api.Multipart
                     file :
                     null;
         }
+
+        /// <summary>
+        /// Writes the JSON representation of the object.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="serializer">The calling serializer.</param>
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotSupportedException();
+        }
     }
 }
